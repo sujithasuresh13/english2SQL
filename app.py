5050b1e8fdd66c30ee26423c5902a6bd8590e559
sujithasuresh13 <31007519+sujithasuresh13@users.noreply.github.com>
2024-05-14 12:35:01 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 from langchain.llms import OpenAI
-from langchain_community.llms import OpenAI
+#from langchain_community.llms import OpenAI
 #from openai import OpenAI
 
 # Streamlit app
