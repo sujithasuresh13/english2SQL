bae75ffc37276bd8ff50f182564b865c8f648942
sujithasuresh13 <31007519+sujithasuresh13@users.noreply.github.com>
2024-05-14 12:40:07 -0700

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ st.title("English to SQL Converter")
 openai_api_key = st.sidebar.text_input('OpenAI API Key', type='password')
 
 # Function to convert English to SQL
-def english_to_sql(openai_api_key, query):
+def english_to_sql(query):
     #OpenAI.api_key = api_key
    
     llm = OpenAI(temperature=0.7, openai_api_key=openai_api_key)
@@ -27,38 +27,45 @@ def english_to_sql(openai_api_key, query):
     # return response['choices'][0]['message']['content']
 
 # Input for English query
-english_query = st.text_area("Enter your English query:")
 
-if st.button("Convert to SQL"):
-    if not openai_api_key:
-        st.error("Please enter your OpenAI API key.")
-    elif not english_query:
-        st.error("Please enter an English query.")
-    else:
-        with st.spinner("Converting..."):
-            try:
-                sql_query = english_to_sql(openai_api_key, english_query)
-                st.success("SQL Query:")
-                st.code(sql_query, language="sql")
-            except Exception as e:
-                st.error(f"Error from the code :: {e}")
+with st.form('my_form'):
+    english_query = st.text_area("Enter your English query:", "top sales in the last quarter")
+    submitted = st.form_submit_button('Submit')
+    if not openai_api_key.startswith('sk-'):
+        st.warning('Please enter your OpenAI API key!', icon='⚠')
+    if submitted and openai_api_key.startswith('sk-'):
+        english_to_sql(english_query)
 
-# Display a footer
-st.markdown(
-    """
-    <style>
-    .footer {
-        position: fixed;
-        bottom: 0;
-        width: 100%;
-        color: white;
-        background-color: #f63366;
-        text-align: center;
-    }
-    </style>
-    <div class="footer">
-        <p>Powered by OpenAI and Streamlit</p>
-    </div>
-    """,
-    unsafe_allow_html=True
-)
+# if st.button("Convert to SQL"):
+#     if not openai_api_key:
+#         st.error("Please enter your OpenAI API key.")
+#     elif not english_query:
+#         st.error("Please enter an English query.")
+#     else:
+#         with st.spinner("Converting..."):
+#             try:
+#                 sql_query = english_to_sql(openai_api_key, english_query)
+#                 st.success("SQL Query:")
+#                 st.code(sql_query, language="sql")
+#             except Exception as e:
+#                 st.error(f"Error from the code :: {e}")
+
+# # Display a footer
+# st.markdown(
+#     """
+#     <style>
+#     .footer {
+#         position: fixed;
+#         bottom: 0;
+#         width: 100%;
+#         color: white;
+#         background-color: #f63366;
+#         text-align: center;
+#     }
+#     </style>
+#     <div class="footer">
+#         <p>Powered by OpenAI and Streamlit</p>
+#     </div>
+#     """,
+#     unsafe_allow_html=True
+# )
