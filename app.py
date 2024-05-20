88d2d450d57f31e481c3d91460f281f4950b3446
sujithasuresh13 <31007519+sujithasuresh13@users.noreply.github.com>
2024-05-20 12:49:51 -0700

    Update app.py

--- app.py
+++ app.py
@@ -4,45 +4,6 @@
 import openai
 import streamlit as st
 
-#from langchain_community.llms import OpenAI
-#from openai import OpenAI
-'''
-# Streamlit app
-st.title("English to SQL Converter")
-
-# API Key input
-# openai_api_key = st.text_input("Enter your OpenAI API Key:", type="password")
-openai_api_key = st.sidebar.text_input('OpenAI API Key', type='password')
-
-# Function to convert English to SQL
-def english_to_sql(query):
-    #OpenAI.api_key = api_key
-    st.warning('englist to sql..', icon='⚠')
-    llm = OpenAI(temperature=0.7, openai_api_key=openai_api_key)
-    st.warning(llm(query))
-    
-    
-    # response = llm.chats.completions.create(
-    #     model="gpt-3.5-turbo",
-    #     messages=[
-    #         {"role": "system", "content": "You are an assistant that converts English to SQL."},
-    #         {"role": "user", "content": query}
-    #     ]
-    # )
-    # return response['choices'][0]['message']['content']
-
-# Input for English query
-english_query = st.text_area("Enter your English query:", "top sales in the last quarter")
-
-if st.button("Convert to SQL"):
-    with st.spinner("Converting..."):
-        st.warning('processing..', icon='⚠')
-        sql_query = english_to_sql(english_query)
-        st.success("SQL Query:")
-        st.code(sql_query, language="sql")
-'''    
-#---------------------------------------
-
 st.title('English to SQL Converter')
 
 api_key = st.sidebar.text_input('Enter your OpenAI API key: ', type='password')
@@ -62,7 +23,7 @@ def generate_response(english_query):
         temperature=0.7,
         max_tokens=512,
     )
-    chat_completion.choices[0].message.content
+    chat_completion.choices[0].messages.content
 
 with st.form('my_form'):
 
@@ -76,53 +37,3 @@ with st.form('my_form'):
         except Exception as e:
             print('Failed to generate : %s', repr(e))
 
-
-#---------------------------------------
-
-# with st.form('my_form'):
-#     english_query = st.text_area("Enter your English query:", "top sales in the last quarter")
-#     submitted = st.form_submit_button('Submit')
-#     if not openai_api_key.startswith('sk-'):
-#         st.warning('Please enter your OpenAI API key!', icon='⚠')
-#     if submitted and openai_api_key.startswith('sk-'):
-#         english_to_sql(english_query)
-
-
-
-
-# if st.button("Convert to SQL"):
-#     if not openai_api_key.startswith('sk-'):
-#         st.warning('Please enter your OpenAI API key!', icon='⚠')
-#     elif not english_query:
-#          st.warning('Please enter query!', icon='⚠')
-#          st.error("Please enter an English query.")
-#     else:
-#         st.warning('converting..', icon='⚠')
-#         with st.spinner("Converting..."):
-#             try:
-#                 st.warning('processing..', icon='⚠')
-#                 sql_query = english_to_sql(english_query)
-#                 st.success("SQL Query:")
-#                 st.code(sql_query, language="sql")
-#             except Exception as e:
-#                 st.error(f"Error from the code :: {e}")
-
-# # Display a footer
-# st.markdown(
-#     """
-#     <style>
-#     .footer {
-#         position: fixed;
-#         bottom: 0;
-#         width: 100%;
-#         color: white;
-#         background-color: #f63366;
-#         text-align: center;
-#     }
-#     </style>
-#     <div class="footer">
-#         <p>Powered by OpenAI and Streamlit</p>
-#     </div>
-#     """,
-#     unsafe_allow_html=True
-# )
