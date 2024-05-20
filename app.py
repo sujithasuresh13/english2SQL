3d193cdf253e2617d1e7de1874783c0ea2422c63
sujithasuresh13 <31007519+sujithasuresh13@users.noreply.github.com>
2024-05-20 12:43:15 -0700

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 from langchain.llms import OpenAI
 #from langchain_community.llms import OpenAI
 #from openai import OpenAI
-
+'''
 # Streamlit app
 st.title("English to SQL Converter")
 
@@ -36,9 +36,44 @@ if st.button("Convert to SQL"):
         sql_query = english_to_sql(english_query)
         st.success("SQL Query:")
         st.code(sql_query, language="sql")
-    
+'''    
+#---------------------------------------
+
+st.title('👩‍💼 Welcome to AI-Powered Resume Builder')
+
+api_key = st.sidebar.text_input('Enter your OpenAI API key: ', type='password')
+
+client = openai.OpenAI(
+    api_key= api_key,
+    base_url="https://api.aimlapi.com/",
+)
+
+def generate_response(english_query):
+    chat_completion = client.chat.completions.create(
+        model="mistralai/Mistral-7B-Instruct-v0.2",
+        messages=[
+            {"role": "system", "content": "You are an assistant that converts English to SQL."},
+            {"role": "user", "content": english_query},
+        ],
+        temperature=0.7,
+        max_tokens=512,
+    )
+    chat_completion.choices[0].message.content
+
+with st.form('my_form'):
+
+    english_query = st.text_input("Enter your English query:", "top sales in the last quarter")
+
+    submitted = st.form_submit_button('Submit')
+   
+    if submitted:
+        try:
+            generate_response(english_query)
+        except Exception as e:
+            print('Failed to generate : %s', repr(e))
 
 
+#---------------------------------------
 
 # with st.form('my_form'):
 #     english_query = st.text_area("Enter your English query:", "top sales in the last quarter")
